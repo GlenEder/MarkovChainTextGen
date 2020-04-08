89e20173665286c4a619d7ac070b583890b43b9a
gleneder <geder@purdue.edu>
2020-04-08 03:14:53 -0400

    Adds parse function

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 
 
 var inputFiles = []         //array of input files
+var textInput = []          //array of text contents of files 
 
 //Adds text file to input files array 
 function addTextFile() {
@@ -25,6 +26,37 @@ function addInputFileToUL(fileName) {
 
 
 //Ouputs markov text from provided input
-function generateText(input) {
+function generateText() {
+
+    inputFiles.forEach( item => {
+        parseFile(item)
+    })
+
+}
+
+
+//parses file into text array  
+function parseFile(file) {
+
+    if (window.File && window.FileReader && Window.FileList && window.Blob) {
+        alert("File Reader APIs not up to date on current browser.")
+        return
+    }
+    
+    //create file reader 
+    var reader = new FileReader()
+
+    if(file.files && file.files[0]) {
+        reader.onload = function (e) {
+            textInput.push(e.target.result)
+        }
+
+        reader.readAsText(file.files[0])
+    }
+    else {
+        alert("Error reading file.")
+    }
+
+
 
 }
