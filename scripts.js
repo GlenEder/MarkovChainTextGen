60661535e6581af7d690f4423ac398e295e56482
gleneder <geder@purdue.edu>
2020-04-08 02:40:37 -0400

    Adds method to add input files

---
+++ scripts.js
@@ -0,0 +1,17 @@
+
+
+var inputFiles = []         //array of input files
+
+//Adds text file to input files array 
+function addText(textFile) {
+
+    inputFiles.push(textFile)
+
+}
+
+
+
+//Ouputs markov text from provided input
+function generateText(input) {
+
+}
